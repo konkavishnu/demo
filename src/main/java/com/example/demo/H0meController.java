5d043f071bd82c3ba8bb46f172cc7e592361c84d
konkavishnu <111354797+konkavishnu@users.noreply.github.com>
2024-04-18 08:35:56 +0530

    Updated 18th april

--- src/main/java/com/example/demo/H0meController.java
+++ src/main/java/com/example/demo/H0meController.java
@@ -1,15 +1,16 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller // --> This is the Keyword is used to declare the class as a controller.
 
 public class H0meController {
 
 
-    @RequestMapping("Home")
-    public String home() {
+    @RequestMapping("/Home")
+    public String home() throws Exception{
 
         System.out.println("Welcome to Home Page");
         return "Home.jsp";
--- src/main/java/com/example/demo/WebApp/Home.jsp
+++ src/main/java/com/example/demo/WebApp/Home.jsp
@@ -1,1 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="contentType" content="text/html; charset=UTF-8">
+<title>Home</title>
+</head>
+<body>
+Hello World
+</body>
+</html>
+
 localhost:8080/Home
